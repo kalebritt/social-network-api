3bfefaff3428418f10e6508b70037a21fa7208d7
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-21 16:19:18 -0600

    completed server

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`That's a good copy on port ${PORT}!`);
+  });
+});
