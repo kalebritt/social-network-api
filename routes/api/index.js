5db1d71a5c463e3f3250c50ef82aa6fae3a88a7f
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-21 17:19:07 -0600

    adding router.use and fixing comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,12 @@
-//const routes
+//express routes
 const router = require("express").Router();
-//userRoutes
-const userRoutes = require('./userRoutes')
-//thoughtRoutes
-
-//router.use
+//userRoute
+const userRoutes = require("./userRoutes");
+//thoughtRoute
+const thoughtRoutes = require("./thoughtRoutes");
+//user route
+router.use("./userRoutes.js");
+//thought route
+router.use("./thoughtRoutes.js");
 
 //module.exports
