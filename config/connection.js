003f328cb5bbe392592adbed810056724ad46b75
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-22 07:52:27 -0600

    added comments and formatting

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,12 @@
+//require mongoose
 const { connect, connection } = require("mongoose");
-
 //mongoose boilerplate
 const connectionString =
   process.env.MONGOD_URI || "mongodb://localhose:27017/socialDB";
-
 //boilerplate mongoose reqs
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 //module.exports
 module.exports = connection;
