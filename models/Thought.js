00203367efeb84cf56115be159ddd5c3ca0ad6b8
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-22 17:04:23 -0600

    completed reactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -27,42 +27,40 @@ const thoughtText = new Schema(
     id: false,
   }
 );
-
-const reactionsSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-});
-
 //reactionSchema
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  //reactionBody here
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  //username
-  username: {
-    type: String,
-    required: true,
-  },
-  //createdAt
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => {
-      return new Date(timestamp).toLocaleString;
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    reactions: [reactionsSchema],
+    //reactionBody here
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    //username
+    username: {
+      type: String,
+      required: true,
+    },
+    //createdAt
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => {
+        return new Date(timestamp).toLocaleString;
+      },
+    },
+
+    reaction: [reactionSchema],
   },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
