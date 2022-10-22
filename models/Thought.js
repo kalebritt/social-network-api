1bd0bb859f61c4fd55703faea09304f86124c7bd
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-22 13:55:47 -0600

    completed thoughtText schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,28 @@
 const { Schema, model, Types } = require("mongoose");
 // const moment = require("moment");
 
-const thoughtText = new Schema({
-  thoughtText: {
-    type: true,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => {
-      return new Date(timestamp).toLocaleString;
+const thoughtText = new Schema(
+  {
+    thoughtText: {
+      type: true,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => {
+        return new Date(timestamp).toLocaleString;
+      },
+      reactions: [reactionsSchema],
     },
   },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
