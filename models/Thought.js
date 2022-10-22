4f4aea0889f4d880eb98891d2e1ae7c59bd4a8e6
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-22 13:41:43 -0600

    added mongoose constant and time stamp code block

---
+++ models/Thought.js
@@ -0,0 +1,10 @@
+// createdAt: {
+//     type: Date,
+//     default: Date.now,
+//     get: (timestamp) => {
+//       return new Date(timestamp).toLocaleString;
+//   },
+
+const { Schema, model, Types } = require("mongoose");
+// const moment = require("moment");
+
