b33687f418243d45dbe30783a052721ab0e48528
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-23 17:02:30 -0600

    commented out routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
-//express routes
-const router = require("express").Router();
-//userRoute
-const userRoutes = require("./userRoutes");
-//thoughtRoute
-const thoughtRoutes = require("./thoughtRoutes");
-//user route
-router.use("/users", userRoutes);
-//thought route
-router.use("/thoughts", thoughtRoutes);
-//module.exports
-module.exports = router;
+// //express routes
+// const router = require("express").Router();
+// //userRoute
+// const userRoutes = require("./userRoutes");
+// //thoughtRoute
+// const thoughtRoutes = require("./thoughtRoutes");
+// //user route
+// router.use("/users", userRoutes);
+// //thought route
+// router.use("/thoughts", thoughtRoutes);
+// //module.exports
+// module.exports = router;
