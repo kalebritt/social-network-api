d2d1b6a75be99fc9f58cd206284f963394e257bd
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-23 16:55:32 -0600

    changed hose to host in config

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const { connect, connection } = require("mongoose");
 //mongoose boilerplate
 const connectionString =
-  process.env.MONGOD_URI || "mongodb://localhose:27017/socialDB";
+  process.env.MONGOD_URI || "mongodb://localhost:27017/studentsDB";
 //boilerplate mongoose reqs
 connect(connectionString, {
   useNewUrlParser: true,
