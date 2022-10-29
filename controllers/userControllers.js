c3745ca412e4dbb517b1a52c945c7e0b04656010
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-29 08:15:35 -0600

    module.exports started in userControllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,11 @@
 //require models
-const { User, Thought } = require('../models')
+const { User, Thought } = require("../models");
 
-//module.exports
+module.exports = {
+    // get all users
+  getUser(req, res) {
+    User.find({}).then((user) => res.json(err));
+  },
+//get single user
+
+};
