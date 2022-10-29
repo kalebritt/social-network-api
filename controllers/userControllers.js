0432fbd2556d36031deb592e03edcc7a7176d182
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-29 08:25:08 -0600

    trying new strategy in userContorllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,11 +1,14 @@
 //require models
 const { User, Thought } = require("../models");
 
-module.exports = {
-    // get all users
-  getUser(req, res) {
-    User.find({}).then((user) => res.json(err));
-  },
-//get single user
+// module.exports = {
+//     // get all users
+//   getUser(req, res) {
+//     User.find({}).then((user) => res.json(err));
+//   },
+// //get single user
+// getSingleUser(req, res) {
+//     User.findOne({ _id: req.params.userId }).populate("thoughts").populate("friends").select("-__v") => !user
+// }
+// };
 
-};
