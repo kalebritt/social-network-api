305210a830a70f9b733d689a7689f17b5f41ae5d
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-30 11:04:07 -0600

    added new thought code block

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -3,5 +3,12 @@ const { Though, User } = require("../models");
 
 // thoughtController
 const thoughtController = {
-    // new thought
-}
+  // new thought
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+//   get thoughts here
+};
