1f37cadebba044941ace58ee638b5ec194babe28
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-10-30 17:18:22 -0600

    uncommented index.js code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,12 @@
 // //express routes
-// const router = require("express").Router();
+const router = require("express").Router();
 // //userRoute
-// const userRoutes = require("./userRoutes");
+const userRoutes = require("./userRoutes");
 // //thoughtRoute
-// const thoughtRoutes = require("./thoughtRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
 // //user route
-// router.use("/users", userRoutes);
+router.use("/users", userRoutes);
 // //thought route
-// router.use("/thoughts", thoughtRoutes);
+router.use("/thoughts", thoughtRoutes);
 // //module.exports
-// module.exports = router;
+module.exports = router;
