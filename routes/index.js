902b6fbf86a28ce0cd75bed0103ad0c182629410
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-01 14:26:41 -0600

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
-//express route here
+// //express route here
+// const router = require("express").Router();
+// //api routes
+// const apiRoutes = require("./api/");
+// //use routes
+// router.use("/", apiRoutes);
+// //think twice, friendo
+// router.use((req, res) => res.send("Think twice, friendo."));
+// //module.exports
+// module.exports = router;
+
 const router = require("express").Router();
-//api routes
-const apiRoutes = require("./api/");
-//use routes
-router.use("/", apiRoutes);
-//think twice, friendo
-router.use((req, res) => res.send("Think twice, friendo."));
-//module.exports
+// Import all of the API routes from /api/index.js
+const apiRoutes = require("./api");
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use("/api", apiRoutes);
+
+router.use((req, res) => res.send("Wrong route!"));
+
 module.exports = router;
