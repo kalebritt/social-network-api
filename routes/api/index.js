1a1040d7ded4c443ebd3fca9afd8239d3e8baad2
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-01 14:27:42 -0600

    some more refactoring

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,21 @@
-// //express routes
+// // //express routes
+// const router = require("express").Router();
+// // //userRoute
+// const userRoutes = require("./userRoutes");
+// // //thoughtRoute
+// const thoughtRoutes = require("./thoughtRoutes");
+// // //user route
+// router.use("/users", userRoutes);
+// // //thought route
+// router.use("/thoughts", thoughtRoutes);
+// // //module.exports
+// module.exports = router;
+
 const router = require("express").Router();
-// //userRoute
 const userRoutes = require("./userRoutes");
-// //thoughtRoute
 const thoughtRoutes = require("./thoughtRoutes");
-// //user route
+
 router.use("/users", userRoutes);
-// //thought route
 router.use("/thoughts", thoughtRoutes);
-// //module.exports
+
 module.exports = router;
