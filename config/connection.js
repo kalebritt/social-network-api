67e0bdf5872cb29d213d96f88cc0bc295d190564
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-01 14:52:20 -0600

    removing comments

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,7 @@
-// //require mongoose
-// const { connect, connection } = require("mongoose");
-// //mongoose boilerplate
-// const connectionString =
-//   process.env.MONGOD_URI || "mongodb://localhost:27017/studentsDB";
-// //boilerplate mongoose reqs
-// connect(connectionString, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// //module.exports
-// module.exports = connection;
-
-const { connect, connection } = require('mongoose');
+const { connect, connection } = require("mongoose");
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
+  process.env.MONGODB_URI || "mongodb://localhost:27017/socialDB";
 
 connect(connectionString, {
   useNewUrlParser: true,
