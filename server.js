b9b5aca05fc6b82067e40168336932b65c4412d9
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-01 14:29:28 -0600

    refactoring server

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-//put your constants here
+// //put your constants here
+// const express = require("express");
+// const db = require("./config/connection");
+// const routes = require("./routes");
+// //port boilerplate
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// //app.use
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(routes);
+// //listen up!
+// db.once("open", () => {
+//   app.listen(PORT, () => {
+//     console.log(`That's a good copy on port ${PORT}!`);
+//   });
+// });
+
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
-//port boilerplate
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-//app.use
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
-//listen up!
+
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`That's a good copy on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
