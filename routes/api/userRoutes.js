e4c32138a554a87b0bf2aaad79b3567fc6596e9a
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-01 14:28:52 -0600

    refactoring

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,25 +1,48 @@
-//require express
+// //require express
+// const router = require("express").Router();
+
+// //crud user
+// const {
+//   getAllUsers,
+//   getSingleUser,
+//   createUser,
+//   updateUser,
+//   deleteUser,
+//   addFriend,
+//   deleteFriend,
+// } = require("../../controllers/userControllers");
+
+// //get all and post
+// router.route("/").get(getAllUsers).post(createUser);
+
+// //get one, put and delete by user Id
+// router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// //delete friend by Id
+// router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+// //module.exports
+// module.exports = router;
+
 const router = require("express").Router();
 
-//crud user
 const {
-  getAllUsers,
+  getUser,
   getSingleUser,
   createUser,
   updateUser,
   deleteUser,
   addFriend,
   deleteFriend,
-} = require("../../controllers/userControllers");
+} = require("../../controllers/userController");
 
-//get all and post
-router.route("/").get(getAllUsers).post(createUser);
+// /api/users GET all and POST
+router.route("/").get(getUser).post(createUser);
 
-//get one, put and delete by user Id
+// /api/users/:userId GET one user, PUT and DELETE by user's ID
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-//delete friend by Id
+// /api/users/:userId/friends/:friendId POST and DELETE a friend by ID
 router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
-//module.exports
 module.exports = router;
