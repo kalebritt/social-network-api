af1e2260afb61ac4dad90264cdf70860fd78eb16
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-01 14:24:49 -0600

    refactor

--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
-//require user
-const User = require("./User");
-//require thought
-const Thought = require('./Thought')
-//module.exports
-module.exports = { User, Thought }
+// //require user
+// const User = require("./User");
+// //require thought
+// const Thought = require('./Thought')
+// //module.exports
+// module.exports = { User, Thought }
+
+const User = require('./User');
+const Thought = require('./Thought');
+
+module.exports = { User, Thought };
